7f62a9a1f19bd48b18c67c551a75592544be30cd
JaehoonSong12 <manual20151276@gmail.com>
2025-01-09 17:47:02 -0500

    Additional scripts has been added

---
+++ dev/02-gpb-backend-run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+else
+    echo -e "\033[1;31mError: ~/.bashrc not found. Exiting...\033[0m"
+    exit 1
+fi
+# /usr
+
+cd gpb-backend
+
+
+
+
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then          # Windows
+    if ! python --version 2>/dev/null && ! python3 --version 2>/dev/null; then
+        INIT_PVM
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then                              # macOS
+    python --version
+    pip --version
+    python3 --version
+    pip3 --version
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then                           # Linux
+    echo "Detected Linux: $OSTYPE"
+    # Add Linux-specific commands here if needed
+else
+    echo "Unsupported OS type: $OSTYPE"
+    exit 1
+fi
+############################################
+########## Shell Script (Scripts) ##########
+############################################
+poetry run gpb
+poetry run gpb_cli
+poetry run gpb_gui
