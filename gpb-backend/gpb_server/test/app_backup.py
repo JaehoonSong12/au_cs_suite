4eb4e0df50f114ef53dbb2a752ff16f4d9ffa331
JaehoonSong12 <manual20151276@gmail.com>
2025-01-15 13:32:50 -0500

    Normal Execution commit: 2025-01-15_13-32-50 updated by Jaehoon

--- gpb-backend/gpb_server/test/app_backup.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def main_fun():    
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-    if response.status_code == 200:
-        print("Response JSON:", response.json())
-    else:
-        print("Failed to fetch data. Status Code:", response.status_code)
